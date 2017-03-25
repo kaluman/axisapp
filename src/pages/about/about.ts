6de0c2d677b466daf92ba16f61f4eea2c21d974b
bcampijr <bcampijr@gmail.com>
2017-03-24 18:32:29 -0700

    Working on the form for submitting the time sheet

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,44 @@
 ﻿import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { AngularFire } from 'angularfire2';
+import firebase from 'firebase';
+import { FormBuilder, Validators } from '@angular/forms';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-    constructor(public navCtrl: NavController) {
+    reportForm: any;
+  
+    constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public alertCtrl: AlertController, public af: AngularFire) {
+        this.reportForm = formBuilder.group({
+            title: ['', Validators.compose([Validators.required])],
+            description: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+        });
+    }
 
+    saveToFirebase() {
+        if (!this.reportForm.valid) {
+            console.log(this.reportForm.value);
+        } else {
+            firebase.database().ref('reports').push({
+                title: this.reportForm.value.title,
+                description: this.reportForm.value.description
+            });
+
+            let alert = this.alertCtrl.create({
+                message: 'Thank You',
+                buttons: [
+                    {
+                        text: "Ok",
+                        role: 'cancel'
+                    }
+                ]
+            });
+            alert.present();
+
+            this.reportForm.reset();
+        }
     }
 }
